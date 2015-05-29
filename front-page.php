79f394d5d43f17c672bf8e2f24d8af6f87a86758
miukimiu <elizabet.oliveira@gmail.com>
2015-05-29 02:05:54 +0100

    Fixed front page

---
+++ front-page.php
@@ -0,0 +1,5 @@
+<p>Aqui slider </p>
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
