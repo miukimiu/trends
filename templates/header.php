dbd9e9372e47c4a60d794a0cbbee1c10d42236f9
miukimiu <elizabet.oliveira@gmail.com>
2015-06-04 23:12:15 +0100

    Material menu

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,12 @@
 <header class="banner" role="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+
+    <div class="material-menu-button">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
     <nav role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
