850e95633ab6150b68429609ea81e3330125a0a5
miukimiu <elizabet.oliveira@gmail.com>
2015-06-12 04:11:08 +0100

    New include header home

--- base.php
+++ base.php
@@ -20,6 +20,13 @@ use Roots\Sage\Wrapper;
     ?>
     <div class="wrap container" role="document">
       <div class="content row">
+
+        <?php
+        if ( is_home() ) {
+            get_template_part('templates/header-home');
+          } 
+        ?>
+
         <main class="main" role="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
---
+++ templates/header-home.php
@@ -0,0 +1,29 @@
+<header class="banner" role="banner">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+
+    <nav role="navigation" class="nav">
+      <a class="menu-trigger">
+        <div class="menu-trigger__lines"></div>
+      </a>
+      <a href="" class="brand-small">logo</a>
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'main-menu']);
+      endif;
+      ?>
+    </nav>
+</header>
+
+<section>
+
+<div class="container">
+  <div class="row">
+          <div class="col-md-6">ok</div>
+           <div class="col-md-6">
+              <div>A</div>
+              <div>B</div>
+           </div>
+        </div>
+  </div>
+</section>
+        
