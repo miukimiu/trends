bfbfb9274791fea942ccc30d4aa5670a78d19749
miukimiu <elizabet.oliveira@gmail.com>
2015-06-12 04:00:29 +0100

    Started slider

--- front-page.php
+++ front-page.php
@@ -1,5 +1,15 @@
 <p>Aqui slider </p>
 <?php while (have_posts()) : the_post(); ?>
+    <div class="row">
+        <div class="col-md-6">ok</div>
+         <div class="col-md-6">
+            <div>A</div>
+            <div>B</div>
+         </div>
+      </div>
+
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+<?php the_posts_navigation(); ?>
