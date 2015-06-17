75fea6f08e7e7ff265d58050fcc0fdaa7114806b
miukimiu <elizabet.oliveira@gmail.com>
2015-06-17 21:01:29 +0100

    Fixed footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,9 @@
 <footer class="footer" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+
+    <div>
+        © COPYRIGHT 2015 CAMILACOELHO.COM - TODOS OS DIREITOS RESERVADOS.   | DESIGN E PROGRAMAÇÃO EVELYNREGLY.COM
+    </div>
   </div>
 </footer>
