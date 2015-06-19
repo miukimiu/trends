d8f142b4647fda7611bccea5f4c940d1f38fccc4
miukimiu <elizabet.oliveira@gmail.com>
2015-06-19 23:21:13 +0100

    Updated header home with videos

--- templates/header-home.php
+++ templates/header-home.php
@@ -25,30 +25,24 @@
       ?>   
     </div>
      <div class="col-sm-4">
-        <?php
-          $counter = 1;
-          $max = 2;
-          // The Loop
-          while ( have_posts() ) : the_post();  
-
-          //queremos apenas um artigo de destaque. Logo limitamos o loop para apenas o último artigo.
-          if ($counter == $max) {
-
-            ?>
-            
-          <div class="featured__item">
-            <?php the_post_thumbnail('featured-small'); ?>
-          </div>
+         <?php
+          if ( get_query_var('paged') ) $paged = get_query_var('paged');  
+          if ( get_query_var('page') ) $paged = get_query_var('page');
            
-          <?php   
-          }
+          $query = new WP_Query( array( 'post_type' => 'videos', 'paged' => $paged, 'showposts' => 1 ) );
+           
+          if ( $query->have_posts() ) : ?>
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?> 
 
-          $counter++;
-          endwhile;
-
-          // Reset Query
-          wp_reset_query();
-        ?>  
+              <div class="featured-portfolio">
+                <?php the_post_thumbnail('featured-small'); ?>
+              </div>
+            
+            <?php endwhile; wp_reset_postdata(); ?>
+            <!-- show pagination here -->
+          <?php else : ?>
+            <!-- show 404 error here -->
+          <?php endif; ?>
 
         <?php
           if ( get_query_var('paged') ) $paged = get_query_var('paged');  
