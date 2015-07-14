0feb3a65168089d45672cc04af84967ee390cbe1
miukimiu <elizabet.oliveira@gmail.com>
2015-07-15 00:45:54 +0100

    Fixed foooter and inserted ionicons

--- templates/footer.php
+++ templates/footer.php
@@ -13,7 +13,7 @@
         <div class="col-xs-7">
             <ul id="social">
                 <li><a target="_blank" href="https://twitter.com/Helenacesarmua"><i class="icon-twitter"></i> </a></li>
-                <li><a target="_blank" href="https://www.facebook.com/helenacesarmakeup"><i class="icon-facebook"></i> </a></li>
+                <li><a target="_blank" href="https://www.facebook.com/helenacesarmakeup"><i class="ion-social-facebook"></i> </a></li>
                 <li><a target="_blank" href="http://uk.pinterest.com/helenacesar/"><i class="icon-pinterest"></i> </a></li>
                 <li><a target="_blank" href="https://www.youtube.com/channel/UCKnq1PELZ46txIm5U0o3AIQ"><i class="icon-youtube"></i> </a></li>
                 <li><a target="_blank" href=""><i class="icon-github-alt"></i></a></li>
