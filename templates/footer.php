df6de17b9f65444f21950c2810a4c8e5577ea61c
miukimiu <elizabet.oliveira@gmail.com>
2015-07-15 00:48:18 +0100

    Footer copyright

--- templates/footer.php
+++ templates/footer.php
@@ -1,29 +1,25 @@
 <footer class="footer" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="container">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
 
-    <div class="row" id="copyright">
+        <div class="row" id="copyright">
+                    
+            <div class="col-xs-5">
                 
-        <div class="col-xs-5">
+                <p>© 2015 Helena Cesar.  </p>
+                                    
+            </div><!--/span_5-->
             
-            <p>© 2015 Helena Cesar.  </p>
-                                
-        </div><!--/span_5-->
-        
-        <div class="col-xs-7">
-            <ul id="social">
-                <li><a target="_blank" href="https://twitter.com/Helenacesarmua"><i class="icon-twitter"></i> </a></li>
-                <li><a target="_blank" href="https://www.facebook.com/helenacesarmakeup"><i class="ion-social-facebook"></i> </a></li>
-                <li><a target="_blank" href="http://uk.pinterest.com/helenacesar/"><i class="icon-pinterest"></i> </a></li>
-                <li><a target="_blank" href="https://www.youtube.com/channel/UCKnq1PELZ46txIm5U0o3AIQ"><i class="icon-youtube"></i> </a></li>
-                <li><a target="_blank" href=""><i class="icon-github-alt"></i></a></li>
-                <li><a target="_blank" href="http://instagram.com/helenacesarmakeup/"><i class="icon-instagram"></i></a></li>
-            </ul>
-        </div><!--/span_7-->
-            
+            <div class="col-xs-7">
+                <ul id="social">
+                    <li><a target="_blank" href="https://twitter.com/Helenacesarmua"><i class="ion-social-twitter"></i> </a></li>
+                    <li><a target="_blank" href="https://www.facebook.com/helenacesarmakeup"><i class="ion-social-facebook"></i> </a></li>
+                    <li><a target="_blank" href="http://uk.pinterest.com/helenacesar/"><i class="ion-social-pinterest"></i> </a></li>
+                    <li><a target="_blank" href="https://www.youtube.com/channel/UCKnq1PELZ46txIm5U0o3AIQ"><i class="ion-social-youtube"></i> </a></li>
+                    <li><a target="_blank" href="http://instagram.com/helenacesarmakeup/"><i class="ion-social-instagram"></i></a></li>
+                </ul>
+            </div><!--/span_7-->
+                
         </div>
-    <div>
-        © COPYRIGHT 2015 HELENACESAR.COM - TODOS OS DIREITOS RESERVADOS
     </div>
-  </div>
 </footer>
