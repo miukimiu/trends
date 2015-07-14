7ea5867f53dd0837b79cc63c842bc1fe06499fe1
miukimiu <elizabet.oliveira@gmail.com>
2015-07-14 23:20:42 +0100

    Fixed front page

--- front-page.php
+++ front-page.php
@@ -10,20 +10,7 @@ while ( have_posts() ) : the_post();
 if($counter>2 || $paged>1)
 {
 ?>
-    <article <?php post_class(); ?>>
-        <header>
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <?php get_template_part('templates/entry-time'); ?>
-        </header>
-
-        <div class="entry-thubmbnail">  
-             <?php the_post_thumbnail('featured'); ?>
-        </div>
-        
-        <div class="entry-summary">
-            <?php the_excerpt(); ?>
-        </div>
-    </article>
+    <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php
 } // endif
 $counter++; 
--- templates/content.php
+++ templates/content.php
@@ -1,9 +1,15 @@
 <article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
+
+    <header>
+        <div class="entry-thubmbnail">
+            <?php the_post_thumbnail('featured'); ?>
+        </div>
+
+        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php get_template_part('templates/entry-meta'); ?>
+    </header>
+
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div>
 </article>
