fe2fb645af9a023d24d774e57a16790f8c06e221
miukimiu <elizabet.oliveira@gmail.com>
2015-07-16 01:26:36 +0100

    Fixed portfolio items

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,20 +1,24 @@
 <div class="row">
     <?php while (have_posts()) : the_post(); ?>
 
-        <article class="portfolio-item col-sm-4">
-            <header>
-                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            </header>
+        <article class="item col-sm-4">
+            <div class="item--overlay">
+              <?php the_post_thumbnail('featured'); ?>
+              <div class="item--overlay__inner">
 
-            <div class="entry-thubmbnail">  
-                 <?php the_post_thumbnail('featured'); ?>
-            </div>
-            
-            <div class="entry-summary">
-                <?php the_excerpt(); ?>
+                <div class="info-outer">
+                  <div class="info">
+
+                    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                  
+                    <?php get_template_part('templates/entry-time'); ?>
+                  
+                    <span class="is-btn-circle"><i class="ion-camera"></i></span>
+                  </div>
+                </div>
+              </div>
             </div>
         </article>
 
     <?php endwhile; ?>
 </div>
-
