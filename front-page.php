aaa47d096be3a4dd95ca29eaec3404383647018e
miukimiu <elizabet.oliveira@gmail.com>
2015-08-06 03:11:49 +0100

    Fixed front page

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@ $counter = 1;
 while ( have_posts() ) : the_post();
 
 //começa a contar a partir do primeiro artigo pois o primeiro já foi impresso em cima. 
-if($counter>2 || $paged>1)
+if($counter>1 || $paged>1)
 {
 ?>
     <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
